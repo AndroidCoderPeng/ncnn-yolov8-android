00958f0ddeb1f82dba7cf287820c8b623ef92e0b
pengxianhong <290677893@qq.com>
2024-10-09 16:01:25 +0800

    更新opencv版本，支持中文直接在c++渲染

--- app/src/main/java/com/pengxh/ncnn/yolov8/TargetDetectView.kt
+++ app/src/main/java/com/pengxh/ncnn/yolov8/TargetDetectView.kt
@@ -19,19 +19,7 @@ class TargetDetectView constructor(context: Context, attrs: AttributeSet) : View
     private val borderPaint by lazy { Paint() }
     private val textRect by lazy { Rect() }
     private val rect by lazy { Rect() }
-    private val classNames = arrayListOf(
-        "三脚架", "三通", "人", "切断阀", "危险告知牌",
-        "压力测试仪", "压力表", "反光衣", "呼吸面罩", "喉箍",
-        "圆头水枪", "安全告知牌", "安全帽", "安全标识", "安全绳",
-        "对讲机", "尖头水枪", "开关", "报警装置", "接头",
-        "施工路牌", "气体检测仪", "水带", "水带_矩形", "流量计",
-        "消火栓箱", "灭火器", "照明设备", "熄火保护", "电线暴露",
-        "电路图", "警戒线", "调压器", "调长器", "贴纸",
-        "跨电线", "路锥", "软管", "过滤器", "配电箱",
-        "长柄阀门", "阀门", "风管"
-    )
-    private val segmentationArray = arrayOf("弯折", "断裂", "烧焦", "磨损", "铁锈", "龟裂")
-    private var results: MutableList<YoloResult> = ArrayList()
+    private var detectResults: MutableList<YoloResult> = ArrayList()
     private var segmentationResults: MutableList<YoloResult> = ArrayList()
     private var textHeight = 0
 
@@ -43,38 +31,32 @@ class TargetDetectView constructor(context: Context, attrs: AttributeSet) : View
         val fontMetrics = textPaint.fontMetrics
         textHeight = (fontMetrics.bottom - fontMetrics.top).toInt()
 
-        backgroundPaint.color = Color.GREEN
         backgroundPaint.style = Paint.Style.FILL
         backgroundPaint.isAntiAlias = true
+        backgroundPaint.color = Color.BLUE
 
-        borderPaint.color = Color.GREEN
         borderPaint.style = Paint.Style.STROKE
-        borderPaint.strokeJoin = Paint.Join.ROUND
-        borderPaint.strokeWidth = 3f.dp2px(context) //设置线宽
+        borderPaint.strokeWidth = 2f.dp2px(context) //设置线宽
         borderPaint.isAntiAlias = true
-    }
-
-    fun updateTargetPosition(results: MutableList<YoloResult>) {
-        this.results = results
-        invalidate()
+        borderPaint.color = Color.BLUE
     }
 
     fun updateTargetPosition(
         segmentationResults: MutableList<YoloResult>, detectResults: MutableList<YoloResult>
     ) {
         this.segmentationResults = segmentationResults
-        this.results = detectResults
+        this.detectResults = detectResults
         postInvalidate()
     }
 
     override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
-        results.forEach {
-            drawTarget(canvas, it, classNames[it.type])
+        detectResults.forEach {
+            drawTarget(canvas, it, LocaleConstant.TARGET_NAMES_ARRAY[it.type])
         }
 
         segmentationResults.forEach {
-            drawTarget(canvas, it, segmentationArray[it.type])
+            drawTarget(canvas, it, LocaleConstant.SEGMENTATION_ARRAY[it.type])
         }
     }
 
@@ -91,7 +73,7 @@ class TargetDetectView constructor(context: Context, attrs: AttributeSet) : View
         )
         canvas.drawRect(textRect, backgroundPaint)
 
-        //画文字。数值是文字左右边距，可酌情调整
+        //画文字
         canvas.drawText(
             label,
             (it.position[0] + textRectWidth / 2).toFloat(),
